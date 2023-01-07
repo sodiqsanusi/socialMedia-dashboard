3c592dc9e37f53969b624a5790e1195a9d1373c9
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-07 02:59:53 +0100

    Done with the basic theming options
    Planning to go deeper into setting the themes. Probably checking the user's preferred theming, then using it on first load

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-var root = document.querySelector(':root');
+const root = document.querySelector('body');
+const themeBtn = document.getElementById('change-theme')
+const themeBtnSelector = document.querySelector('#change-theme span')
 
-
-let darkTheme = {
-  bgColor: 'hsl(230, 17%, 14%)',
-  topBg: 'hsl(232, 19%, 15%)',
-  cardBg: 'hsl(228, 28%, 20%)',
-  subText: 'hsl(228, 34%, 66%)',
-  mainText: 'hsl(0, 0%, 100%)',
-}
-
-let lightTheme = {
-  bgColor: 'hsl(0, 0%, 100%)',
-  topBg: 'hsl(225, 100%, 98%)',
-  cardBg: 'hsl(227, 47%, 96%)',
-  subText: 'hsl(228, 12%, 44%)',
-  mainText: 'hsl(230, 17%, 14%)',
+themeBtn.onclick = () => {
+  themeBtnSelector.classList.toggle('light')
+  root.classList.toggle('light')
 }
